49232b1bd574329634d86fdcae99afd7d774d36f
XanJan <annamajberger@gmail.com>
2023-11-29 13:17:47 +0100

    added comments

--- LAB3/triggers.sql
+++ LAB3/triggers.sql
@@ -1,14 +1,18 @@
 CREATE FUNCTION on_register() RETURNS trigger AS $$
 BEGIN
+--Checks if studnet is already in registration or watinglist and throws an exception
   IF (EXISTS (SELECT * FROM Registrations WHERE student = NEW.student AND course = NEW.course)) THEN
     RAISE EXCEPTION 'Student already registered or waiting.';
   END IF;
+
   -- CHECK IF STUDENT IS ALLOWED TO REGISTER FOR A COURSE :)
   IF (EXISTS (SELECT * FROM Prerequisites WHERE Prerequisites.course = NEW.course)) THEN
+  --Checks if student has passed all the courses that is in prerequisites
     IF (NOT EXISTS (SELECT * FROM Prerequisites JOIN PassedCourses ON (required_course = PassedCourses.course) WHERE (Prerequisites.course = NEW.course  AND student = NEW.student))) THEN
       RAISE EXCEPTION 'Student has not taken the right courses';
     END IF;
   END IF;
+  --If there is 
   IF ((SELECT capacity FROM LimitedCourses WHERE code = NEW.course) <= (SELECT COUNT(*) FROM Registrations WHERE course = NEW.course)) THEN
     INSERT INTO WaitingList VALUES(NEW.student, NEW.course, 1);
     RETURN OLD;
@@ -16,6 +20,8 @@ BEGIN
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+
 
 CREATE FUNCTION on_waitinglist_insert() RETURNS trigger AS $$
 DECLARE
@@ -33,6 +39,8 @@ BEGIN
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+
 
 CREATE TRIGGER on_register BEFORE INSERT ON Registered
     FOR ROW EXECUTE PROCEDURE on_register();
